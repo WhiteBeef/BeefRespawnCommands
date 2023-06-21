bc200c47862c6f6c7e0ce55e4e10452498aac242
White Beef <whitebeef.official@gmail.com>
2023-06-21 18:35:33 +0300

    Removed redis, upstream to 1.19.4

--- src/main/java/ru/whitebeef/respawncommands/RespawnCommands.java
+++ src/main/java/ru/whitebeef/respawncommands/RespawnCommands.java
@@ -1,17 +1,18 @@
 package ru.whitebeef.respawncommands;
 
-import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.whitebeef.beeflibrary.BeefLibrary;
 import ru.whitebeef.beeflibrary.commands.AbstractCommand;
 import ru.whitebeef.beeflibrary.commands.SimpleCommand;
 import ru.whitebeef.beeflibrary.placeholderapi.PAPIUtils;
+import ru.whitebeef.beeflibrary.utils.ScheduleUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.List;
 public final class RespawnCommands extends JavaPlugin implements Listener {
 
     private final List<Command> commands = new ArrayList<>();
+    private final List<String> firstJoinCommands = new ArrayList<>();
 
     @Override
     public void onEnable() {
@@ -39,6 +41,7 @@ public final class RespawnCommands extends JavaPlugin implements Listener {
 
     public void reload() {
         commands.clear();
+        firstJoinCommands.clear();
         ConfigurationSection section = getConfig().getConfigurationSection("commands");
         for (String namespace : section.getKeys(false)) {
             if (section.getBoolean(namespace + ".enable")) {
@@ -46,23 +49,28 @@ public final class RespawnCommands extends JavaPlugin implements Listener {
                         section.getString(namespace + ".permission")));
             }
         }
+        firstJoinCommands.addAll(getConfig().getStringList("firstJoinCommands"));
     }
 
-    @EventHandler
-    public void onPlayerDead(PlayerDeathEvent event) {
-        Player player = event.getPlayer();
-        BeefLibrary.jedisSet(this, player.getUniqueId().toString(), String.valueOf(System.currentTimeMillis()));
-    }
 
     @EventHandler
-    public void onRespawn(PlayerPostRespawnEvent event) {
-        if (BeefLibrary.jedisGet(this, event.getPlayer().getUniqueId().toString()) == null) {
+    public void onRespawn(PlayerRespawnEvent event) {
+        if (event.getRespawnReason() == PlayerRespawnEvent.RespawnReason.DEATH) {
             return;
         }
         Player player = event.getPlayer();
-        BeefLibrary.jedisDel(this, player.getUniqueId().toString());
-        commands.stream().filter(command -> command.hasPermission(player)).forEach(command ->
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PAPIUtils.setPlaceholders(player, command.getCommand())));
+        ScheduleUtils.runTaskLater(this, () -> commands.stream().filter(command -> command.hasPermission(player)).forEach(command ->
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PAPIUtils.setPlaceholders(player, command.getCommand()))), 1L);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (!player.hasPlayedBefore()) {
+            firstJoinCommands.forEach(command ->
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PAPIUtils.setPlaceholders(player, command)));
+        }
+
     }
 
 }
